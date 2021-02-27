b4fbc0eac831c0e24c6a61fcccf202be011bf53f
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-02-27 00:17:35 -0300

    style: navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import './styles/global.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div id="store">
+      <ul>
+        <li>Akita's Shopping Cart</li>
+        <li2>by Rafael Sanchez Souza</li2>
+      </ul>
+      <body>{/* <h1>Hello World!</h1> */}</body>
     </div>
   );
 }
