a0bffc781ada5acd26f30a891ca235eb6643c5f6
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-02-27 01:19:24 -0300

    feat: fetch products from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import './styles/global.css';
+import api from './services/api';
 
 function App() {
+  const [storeProducts, setStoreProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('').then((response) => {
+      setStoreProducts(response.data);
+    });
+    // return () => {
+    //   cleanup;
+    // };
+  }, []);
+
   return (
     <div id="store">
       <ul>
         <li>Akita's Shopping Cart</li>
-        <li2>by Rafael Sanchez Souza</li2>
+        <li className="author">by Rafael Sanchez Souza</li>
       </ul>
-      <body>{/* <h1>Hello World!</h1> */}</body>
     </div>
   );
 }
